899420e61c971241aa5c7de377f689b0e43b9141
Valdir de Souza Junior <vsj@ic.ufal.br>
2019-08-02 14:13:43 -0300

    .

--- usart_except/main.asm
+++ usart_except/main.asm
@@ -6,6 +6,8 @@
 	jmp handle_INT2
 .org OC1Aaddr
 	jmp OC1A_Interrupt
+.org 0x0028
+    jmp USART_TX_Complete
 .def temp = r16
 .def temp2 = r22
 .def temp3 = r24
@@ -30,6 +32,19 @@
 .equ B = 2
 .equ A = 3
 
+
+disable_transmit_interrupt:
+	push temp
+	ldi temp, 0
+	sts UCSR0B, temp; 
+	pop temp
+	ret
+enable_transmit_interrupt:
+	push temp
+	ldi temp, (1<<TXCIE0)|(1 << TXEN0)
+	sts UCSR0B, temp; enable transmit and transmit interrupt
+	pop temp
+	ret
 
 startTimer:
 	ldi temp, ((WGM>> 2) << WGM12)|(PRESCALE << CS10)
@@ -140,6 +155,11 @@ fecha:
 	ret
 
 
+USART_TX_Complete:
+	ldi temp4, 'A'
+	sts UDR0, temp4
+	reti
+
 OC1A_Interrupt:
 cli
 	subi contador, -1
@@ -179,13 +199,15 @@ cli
 	
 handle_INT2:
 	cli 
+	in temp, PIND
+;	call liga_buzzer
 	call delay20ms;Debouncing
 	;call delay20ms;
 	;0 do Elevador = PD4 ; PCINT20
 	;1 do Elevador = PD5 ; PCINT21
 	;2 do Elevador = PD6 ; PCINT22
 	;Abrir do Elevador = PD7; PCINT23
-	in temp, PIND
+
 	
 	
 	sbrc temp,4 
@@ -204,21 +226,21 @@ handle_INT2:
 	jmp end_handle_int2
 	botao_chamar_I0_pressionado:
 		ldi andar, 0
-		call atualiza_display
+;		call atualiza_display
 		sbr botoes, ( 1<<botoesI0)
 		jmp end_handle_int2
 	botao_chamar1_in_pressionado:
 		ldi andar, 1
-		call atualiza_display
-		sbr botoes, ( 1<<botoesI1)
+;		call atualiza_display
+;		sbr botoes, ( 1<<botoesI1)
 		jmp end_handle_int2
 	botao_chamar2_in_pressionado:
 		ldi andar, 2
-		call atualiza_display
-		sbr botoes, ( 1<<botoesI2)
+;		call atualiza_display
+	;	sbr botoes, ( 1<<botoesI2)
 		jmp end_handle_int2
 	botao_abrir_pressionado:
-		sbrs flags, flagsEstado
+	;	sbrs flags, flagsEstado
 		call liga_led
 		call abre
 		jmp end_handle_int2
@@ -229,6 +251,7 @@ handle_INT2:
 
 
 handle_INT0:
+	
 	cli ; TODO: Só desligar essa interrupção
 	; Fechar do Elevador = PB0; PCINT0
 	; Chamar 0 = PB2; PCINT2
@@ -248,20 +271,21 @@ handle_INT0:
 
 	botao_chamar0_ext_pressionado:
 		ldi andar, 0
-		call atualiza_display
-		sbr botoes, ( 1<<botoesE0)
+	;	call atualiza_display
+	;	sbr botoes, ( 1<<botoesE0)
 		jmp end_handle_int0
 	botao_chamar1_ext_pressionado:
 		ldi andar, 1
-		call atualiza_display
-		sbr botoes, ( 1<<botoesE1)
+	;	call atualiza_display
+	;	sbr botoes, ( 1<<botoesE1)
 		jmp end_handle_int0
 	botao_chamar2_ext_pressionado:
 		ldi andar, 2
-		call atualiza_display
-		sbr botoes, ( 1<<botoesE2)
+	;	call atualiza_display
+	;	sbr botoes, ( 1<<botoesE2)
 		jmp end_handle_int0
 	botao_fechar_pressionado:
+		call apaga_buzzer
 		call apaga_led
 		jmp end_handle_int0
 
@@ -271,6 +295,19 @@ handle_INT0:
 reset:
 
 	cli
+
+
+	.equ UBRRvalue = 103
+	;initialize USART
+	ldi temp, high (UBRRvalue) ;baud rate
+	sts UBRR0H, temp
+	ldi temp, low (UBRRvalue)
+	sts UBRR0L, temp
+	;8data, 1 stop, no parity
+	ldi temp, (3<<UCSZ00)
+	sts UCSR0C, temp
+
+	;call enable_transmit_interrupt
 
 	#define CLOCK 16.0e6 ;clock speed
 	.equ PRESCALE = 0b100 ;/256 prescale
@@ -320,7 +357,7 @@ reset:
 	; Enables PCINT 23 TO 20
 	ldi temp, 0b11110000
 	sts PCMSK2, temp 
-	sei
+	
 
 	;Stack initialization
 	ldi temp, low(RAMEND)
@@ -333,6 +370,128 @@ reset:
 	ldi andar, 0        ; Andar = 0
 	ldi flags, 0b00000001 ; Porta fechada e Parado.
 	call fecha
-
+	ldi temp4, 'S'
+	sei
+	sts UDR0, temp4
 	main:
-		rjmp PC
+	call atualiza_display
+	rjmp main
+;TODO: Ficar printando andar no display usando PIND/B  e PORTD/B
+	; IF flagEstado==1 (Em movimento)
+	sbrc flags, flagsEstado
+	rjmp estado_em_movimento
+		estado_parado:
+			sbrs flags, flagsPortaFechada ; 
+			rjmp if_porta_aberta
+			if_porta_fechada:
+				cp destino, andar
+				breq if_parado_porta_aberta_ou_fechada
+				if_porta_fechada_destino_diff_atual:
+					sbr flags, (1 << flagsEstado) ; Ativa Estado em Movimento
+					rcall startTimer
+					rjmp if_parado_porta_aberta_ou_fechada	
+
+	rjmp main
+		estado_em_movimento:
+			rjmp main;
+		if_porta_aberta:
+		if_parado_porta_aberta_ou_fechada:
+			; Switch(andar)
+			cpi andar, 0
+			breq andar_0
+			cpi andar, 1
+			breq andar_1
+			cpi andar, 2
+			breq andar_2
+			rjmp main;
+
+andar_0:
+	; if (I0 | E0)
+	; botoes = r18  ;0b  0-E0-E1-E2 0-I0-I1-I2
+	sbrc botoes, botoesI0  ; IF I0==1
+	rjmp andar_0_I0_E0
+	sbrc botoes, botoesE0
+	rjmp andar_0_I0_E0
+ 
+	sbrc botoes, botoesI2  ; IF I2==1
+	rjmp andar_0_I2_E2
+	sbrc botoes, botoesE2
+	rjmp andar_0_I2_E2
+
+	sbrc botoes, botoesI1  ; IF I1==1
+	rjmp andar_0_I1_E1
+	sbrc botoes, botoesE1
+	rjmp andar_0_I1_E1
+	rjmp main
+	andar_0_I0_E0:
+		call abre
+		cbr botoes, (1 << botoesE0)|(1<<botoesI0) ; Dá Clear nos botões E0(bit6) e I0(bit2). 
+		;Produz uma máscara
+		; Fazendo shift em cada posição de BIT.  Depois zera onde é 1.		
+		rjmp main
+	andar_0_I1_E1:
+	andar_0_I2_E2:
+		ldi destino, 1
+		rjmp main
+	
+andar_1:
+	; botoes = r18  ;0b  0-E0-E1-E2 0-I0-I1-I2
+	sbrc botoes, botoesI1  ; IF I1==1
+	rjmp andar_1_I1_E1
+	sbrc botoes, botoesE1
+	rjmp andar_1_I1_E1
+ 
+	sbrc botoes, botoesI2  ; IF I2==1
+	rjmp andar_1_I2_E2
+	sbrc botoes, botoesE2
+	rjmp andar_1_I2_E2
+
+	sbrc botoes, botoesI1  ; IF I1==1
+	rjmp andar_1_I1_E1
+	sbrc botoes, botoesE1
+	rjmp andar_1_I1_E1
+
+	andar_1_I1_E1:
+		call abre
+		ldi temp3, (1 << botoesE1)|(1<<botoesI1)
+		cbr botoes, (1 << botoesE1)|(1<<botoesI1) ; Dá Clear nos botões E1 e I1. 
+		;Produz uma máscara
+		; Fazendo shift em cada posição de BIT.  Depois zera onde é 1.		
+		rjmp main
+	andar_1_I2_E2:
+		ldi destino, 2
+		rjmp main
+	andar_1_I0_E0:
+		ldi destino, 0
+		rjmp main
+
+
+andar_2:
+	; botoes = r18  ;0b  0-E0-E1-E2 0-I0-I1-I2
+	sbrc botoes, botoesI2  ; IF I2==1
+	rjmp andar_2_I2_E2
+	sbrc botoes, botoesE2
+	rjmp andar_2_I2_E2
+ 
+	sbrc botoes, botoesI2  ; IF I2==1
+	rjmp andar_2_I2_E2
+	sbrc botoes, botoesE2
+	rjmp andar_2_I2_E2
+
+	sbrc botoes, botoesI1  ; IF I1==1
+	rjmp andar_2_I1_E1
+	sbrc botoes, botoesE1
+	rjmp andar_2_I1_E1
+
+	andar_2_I2_E2:
+		call abre
+		cbr botoes, (1 << botoesE2)|(1<<botoesI2) ; Dá Clear nos botões E1 e I1. 
+		;Produz uma máscara
+		; Fazendo shift em cada posição de BIT.  Depois zera onde é 1.		
+		rjmp main
+	andar_2_I1_E1:
+	andar_2_I0_E0:
+		ldi destino, 1
+		rjmp main
+
+	
